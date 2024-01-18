2089302c8a0ac2b268084b0d6e5cfaf6641bf600
William Roger <williamroger.frontend@gmail.com>
2024-01-18 20:02:05 -0300

    css-in-js utilizando styled components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,23 @@
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
 import { Groups } from '@screens/Groups'
 
+import theme from './src/theme'
+
 export default function App() {
-  return <Groups />
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
+  return (
+    <ThemeProvider theme={theme} >
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent={true}
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  )
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Header } from '@components/Header';
+
+import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <Header />
+
+      <Highlight 
+        title='Turmas' 
+        subtitle='jogue com a sua turma'
+      />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+        contentContainerStyle={!groups.length && { flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Que tal cadastrar a sua primeira turma?" 
+          />
+        )}
+      />
+      
+      <Button title="Criar nova turma" />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
