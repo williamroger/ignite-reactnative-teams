2692a158414bae03832242af09594a06e837dd62
William Roger <williamroger.frontend@gmail.com>
2024-01-31 22:21:55 -0300

    adicionada nova tela newGroup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'react-native'
-import { ThemeProvider } from 'styled-components'
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Loading } from '@components/Loading'
-import { Groups } from '@screens/Groups'
+import { Loading } from '@components/Loading';
+import { NewGroup } from '@screens/NewGroup';
 
-import theme from './src/theme'
+import theme from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent={true}
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   )
 }
