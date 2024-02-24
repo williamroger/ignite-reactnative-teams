41ba87fb0ae621e8c2a46d54a321b23e2be39e74
William Roger <williamroger.frontend@gmail.com>
2024-02-24 20:06:04 -0300

    estruturando rotas da aplicação

--- App.tsx
+++ App.tsx
@@ -3,8 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Loading } from '@components/Loading';
-import { NewGroup } from '@screens/NewGroup';
-import { Players } from '@screens/Players';
+import { Routes } from './src/routes';
 
 import theme from './src/theme';
 
@@ -18,7 +17,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent={true}
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   )
 }
