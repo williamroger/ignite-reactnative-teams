8582df2283068feb690ec0a3d0a7528e00813022
William Roger <williamroger.frontend@gmail.com>
2024-02-24 23:28:28 -0300

    adicionada tipagem de rotas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,23 @@
-import { Header } from '@components/Header';
-
-import { Container } from './styles';
-import { Highlight } from '@components/Highlight';
-import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+import { Container } from './styles';
+
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -34,7 +42,10 @@ export function Groups() {
         )}
       />
       
-      <Button title="Criar nova turma" />
+      <Button 
+        title="Criar nova turma" 
+        onPress={handleNewGroup}
+      />
     </Container>
   );
 }
