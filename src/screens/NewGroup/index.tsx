6e902a2dc211b0856f0e634f2bea61d3b358cb90
William Roger <williamroger.frontend@gmail.com>
2024-02-29 02:37:42 -0300

    implementada a navegação básica entre as rotas

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,15 @@ import { Container, Content, Icon } from './styles';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { Highlight } from '@components/Highlight';
+import { useNavigation } from '@react-navigation/native';
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('players', { group: 'Rocket' });  
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -18,6 +25,7 @@ export function NewGroup() {
         <Input />
 
         <Button 
+          onPress={handleNewGroup}
           title="Criar"
           style={{ marginTop: 20 }}
         />
