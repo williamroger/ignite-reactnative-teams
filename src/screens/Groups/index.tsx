52fba04fa6f4ceb0bddb1d8c304c0d64992e76ce
William Roger <williamroger.frontend@gmail.com>
2024-04-17 18:16:00 -0300

    abrindo turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -29,6 +29,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group });
+  }
+
   useFocusEffect(useCallback(() => {
     fetchGroups();
   }, []));
@@ -46,7 +50,10 @@ export function Groups() {
         data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => (
-          <GroupCard title={item} />
+          <GroupCard 
+            title={item} 
+            onPress={() => handleOpenGroup(item)}
+          />
         )}
         contentContainerStyle={!groups.length && { flex: 1}}
         ListEmptyComponent={() => (
